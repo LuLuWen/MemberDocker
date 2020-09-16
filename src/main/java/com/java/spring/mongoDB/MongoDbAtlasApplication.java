f1c2906f0d023bc0c536fc554584aae8c13dee14
LuLuWen <38238159+LuLuWen@users.noreply.github.com>
2020-09-16 13:42:01 +0800

    Update MongoDbAtlasApplication.java

--- src/main/java/com/java/spring/mongoDB/MongoDbAtlasApplication.java
+++ src/main/java/com/java/spring/mongoDB/MongoDbAtlasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class MongoDbAtlasApplication {
 
 	public static void main(String[] args) {
